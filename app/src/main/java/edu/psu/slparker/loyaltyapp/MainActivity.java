4f8227cb188f17dc8d1a14d7672aacf6afe5314e
Samantha <slparker0718@yahoo.com>
2018-11-04 14:58:56 -0500

    Login Skeleton Welcome Skeleton Navigation Item Skeleton

--- app/src/main/java/edu/psu/slparker/loyaltyapp/MainActivity.java
+++ app/src/main/java/edu/psu/slparker/loyaltyapp/MainActivity.java
@@ -1,13 +1,27 @@
 package edu.psu.slparker.loyaltyapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    public Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        login = (Button) findViewById(R.id.button_login);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Welcome.class));
+            }
+        });
     }
 }
